91e99e438ae395c43544562fb768537d6dd52658
HHfguzje <hoang03.it@gmail.com>
2024-03-22 09:42:51 +0700

    Completete Discovery Server

---
+++ Discovery_Server/src/test/java/com/ltf/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltf.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
