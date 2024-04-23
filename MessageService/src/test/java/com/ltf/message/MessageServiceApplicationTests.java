e5471c0a68f5c6795f9d163f465b4dddd8534b30
lhoang1910 <hoang03.it@gmail.com>
2024-04-23 23:32:58 +0700

    add messageservice to ltf

---
+++ MessageService/src/test/java/com/ltf/message/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltf.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
