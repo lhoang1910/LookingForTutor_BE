dfc4820786db8119cdb59ef277046262fd2a74e4
HHfguzje <hoang03.it@gmail.com>
2024-05-03 22:10:20 +0700

    xoa

--- MessageService/src/main/java/com/ltf/message/repository/BoxChatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ltf.message.repository;
-
-import com.ltf.message.entities.BoxChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoxChatRepository extends JpaRepository<BoxChat, Long> {
-}
